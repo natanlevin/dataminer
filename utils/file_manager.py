70b655df8f1a8752eccb1069bc9e931f2a9745a9
natanlevin <natanlevin@gmail.com>
2017-09-27 09:11:41 +0300

    initialization

---
+++ utils/file_manager.py
@@ -0,0 +1,12 @@
+from utils.config import *
+
+
+class FileManger(object):
+    def __init__(self):
+        pass
+
+    def save_data(self):
+        raise NotImplementedError
+
+    def load_data(self):
+        raise NotImplementedError
